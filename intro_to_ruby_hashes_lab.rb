4d78f6e6b13ca745d493c6a09c766dd1577e0b36
Johnathan Pruitt <pruittj2000@yahoo.com>
2020-02-19 10:00:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,7 +20,7 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  hashin = {key: value}
+  hashin = {:key => value}
 end
 
 def read_from_hash(hash, key)
