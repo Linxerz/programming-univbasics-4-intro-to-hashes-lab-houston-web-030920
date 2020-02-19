b483b684dbf54d55c4b08c2b45fb8ba4938c5c8e
Johnathan Pruitt <pruittj2000@yahoo.com>
2020-02-19 09:53:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,13 @@
 def new_hash
   # return an empty hash
+  hsh = Hash.new
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  hashin = Hash.new {
+    key: => "value"
+  }
 end
 
 def pioneer
